e33870887c3539e7590ba0eb7ca570099f4c7904
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-21 13:01:05 +0530

    code

--- DrugDisc_Backend/drugdisc_app/views.py
+++ DrugDisc_Backend/drugdisc_app/views.py
@@ -6,19 +6,14 @@ from .serializers import InputDataSerializer
 
 class GenerateAlternateDrug(APIView):
     def post(self, request):
-        # Query the database for the first instance of InputData
         input_data = InputData.objects.first()
 
         if input_data:
-            # If an instance is found, retrieve the alternate_drug field
             alternate_drug = input_data.alternate_drug
-
-            # Create a dictionary response with the alternate_drug field
             response_data = {
                 'alternate_drug': alternate_drug
             }
 
             return Response(response_data, status=status.HTTP_200_OK)
         else:
-            # If no instance is found, return an appropriate error response
             return Response({'error': 'No InputData found'}, status=status.HTTP_404_NOT_FOUND)
